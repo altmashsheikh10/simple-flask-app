a132fba548360010faac6d613ab97fd980837500
Altmash Sheikh <altmashsheikh10@gmail.com>
2024-12-11 16:48:12 +0530

    changing

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
 app = Flask(__name__)
-
+name = 'Rahul'
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return f"Hello {name}!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
